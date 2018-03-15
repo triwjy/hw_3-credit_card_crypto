e011446ff87bce46cc3e386a3e23e4c63cab351f
triwjy <ztriwijaya@gmail.com>
2018-03-15 17:04:36 +0800

    Luhn Validator

--- credit_card.rb
+++ credit_card.rb
@@ -3,18 +3,26 @@ require 'json'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
     # TODO: initialize the instance variables listed above
   end
 
   # returns json string
   def to_json
     {
-      # TODO: setup the hash with all instance vairables to serialize into json
+      # TODO: setup the hash with all instance variables to serialize into json
+      :number => @number
+      :expiration_date => @expiration_date
+      :owner => @owner
+      :credit_network => @credit_network
     }.to_json
   end
 
@@ -26,6 +34,7 @@ class CreditCard
   # return a new CreditCard object given a serialized (JSON) representation
   def self.from_s(card_s)
     # TODO: deserializing a CreditCard object
+    
   end
 
   # return a hash of the serialized credit card object
--- luhn_validator.rb
+++ luhn_validator.rb
@@ -3,9 +3,13 @@ module LuhnValidator
   # arguments: none
   # assumes: a local String called 'number' exists
   # returns: true/false whether last digit is correct
-  def validate_checksum
-    nums_a = number.to_s.chars.map(&:to_i)
-
+  def validate_checksum(number)
+    nums_a = number.to_s.chars.map(&:to_i).reverse
+    valid=nums_a.map.with_index{|n,i| i.odd? ? n*2>9 ? n*2-9 : n*2 :n}.reduce(0, :+)
+    (valid%10==0)
     # TODO: use the integers in nums_a to validate its last check digit
+  end
+
+  def control_digit
   end
 end
